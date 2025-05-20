64dbb24128a249d0460cc3ab693fbb7f2f16a1fc
Arisveus <arielmatteibisatto12@gmail.com>
2025-05-20 08:47:23 -0300

    20/05/2025
    Bão

--- resources/views/keepinho/editar.blade.php
+++ resources/views/keepinho/editar.blade.php
@@ -2,14 +2,16 @@
 <h2>Seja Bem vindo ao editor do keepinho
 pessoal (melhor do que o firefox).</h2>
 <hr>
-< action="{{ route('keep.editarGravar') }}" method="post">
+<form action="{{ route('keep.editarGravar') }}" method="post">
     @csrf
-    @method('PUT');
+    @method('PUT')
 
     <input type="hidden" name="id" value="{{ $nota->id }}">
-    <input type="text" name="titulo">{{ $nota->titulo }}</input>
-    <textarea name="texto" id="" cols="80" rows="0">{{ $nota->texto }}</textarea>
+    <input type="text" name="titulo" placeholder="{{ old('titulo', $nota->titulo) }}"></input>
     <br>
+    <br>
+    <textarea name="texto" id="" cols="80" rows="0">{{ old('texto', $nota->texto) }}</textarea>
+    <br><br>
     <input type="submit" value="Editar Nota">
 </form>
 
--- resources/views/keepinho/index.blade.php
+++ resources/views/keepinho/index.blade.php
@@ -5,28 +5,37 @@ pessoal (melhor do que o google).</h2>
 @if ($errors->any())
     <div style="color:red">
     <h3><b>Erro!</b></h3>
+
+    <ul>
+        @foreach ($errors->all() as $err)
+        <li>{{ $err }}</li>
+        @endforeach
+    </ul>
     </div>
 @endif
+
 <form action="{{ route('keep.gravar') }}" method="post">
     @csrf
-    <input type="text" name="titulo" placeholder="Titulo da nota"></input>
+    <input type="text" name="titulo" placeholder="Titulo da nota" value="{{ old('titulo') }}"></input>
     <br><br>
-    <textarea name="texto" id="" cols="80" rows="0" placeholder="Digite a nota aqui"></textarea>
-    <br>
+    <textarea name="texto" id="" cols="80" rows="0" placeholder="Digite a nota aqui">{{ old('texto') }}</textarea>
+    <br><br>
     <input type="submit" value="Gravar Nota">
 </form>
 <hr>
+
     @foreach ($notas as $nota)
         <div style="border:1px dashed green;padding:2px;background-color:whitesmoke; ">
             <b>{{ $nota->titulo }}</b>
-            <br>
+            <br><br>
             {{ $nota->texto }}
-            <br> 
+            <br> <br>
             <a href="{{ route('keep.editar' , $nota->id) }}">Editar</a>
-
+            <br>
             <form action="{{ route('keep.apagar', $nota->id) }}" method="POST">
             @method('DELETE')
             @csrf
+            <br>
             <input type="submit" value="Apagar">
             </form>
         </div>
