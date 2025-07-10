8eb35d7f2035db2f97051c863634aa0706c2c827
Lucas Gabriel <142439949+LucasGabriel408@users.noreply.github.com>
2025-07-10 11:13:58 -0300

    Comment Factory
    Salvando o projeto funcionando por segurança. Próximos passos são a construção do front-end.

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,16 +2,15 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Testing\Fakes\Fake;
-use App\Models\Post;
-use App\Models\Category;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
  */
-class PostFactory extends Factory
+class CommentFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -21,10 +20,9 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => fake()->realText(15),
-            'content' => fake()->realText(1000),
-            'category_id' => Category::inRandomOrder()->value('id'),
+            'content' => fake()->realText(500),
             'user_id' => User::inRandomOrder()->value('id'),
+            'post_id' => Post::inRandomOrder()->value('id'),
         ];
     }
 }
